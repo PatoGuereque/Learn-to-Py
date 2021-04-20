1ba928d322aa8410b52703d3399e07a951518dbe
user189355 <user189355@74-80-250-254.ic2net.net>
2021-04-20 12:40:45 -0700

    Ya funciona la funcionalidad de verse como un popUp

---
+++ Learn-to-py/popUpEditar_i.swift
@@ -0,0 +1,37 @@
+//
+//  popUpEditar_i.swift
+//  Learn-to-py
+//
+//  Created by user189355 on 4/20/21.
+//
+
+import UIKit
+
+class popUpEditar_i: UIViewController {
+
+    @IBOutlet weak var tfNombreVar: UITextField!
+    @IBOutlet weak var lb_i: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func guardarNombre(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
