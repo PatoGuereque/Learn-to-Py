3e5de5e6a8658ad0ae314e305a91e7286c280d90
JavierSanchezVazquezdeMercado <a00517066@itesm.mx>
2021-04-21 14:44:49 -0700

    Se hizo el diseño de la vista del bucle for.

---
+++ Learn-to-py/ViewControllerCicloFor.swift
@@ -0,0 +1,47 @@
+//
+//  ViewControllerCicloFor.swift
+//  Learn-to-py
+//
+//  Created by user189625 on 4/20/21.
+//
+
+import UIKit
+
+class ViewControllerCicloFor: UIViewController {
+
+    @IBAction func onEditVariable(_ sender: UIButton) {
+    }
+    
+    @IBAction func onEditIterable(_ sender: UIButton) {
+    }
+    
+    @IBAction func onEditCodigo(_ sender: UIButton) {
+    }
+    
+    @IBAction func onAnterior(_ sender: UIButton) {
+    }
+    
+    @IBAction func onPausa(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSiguiente(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
